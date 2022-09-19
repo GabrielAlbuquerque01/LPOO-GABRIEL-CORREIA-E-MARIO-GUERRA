57fa6a9034fc93a779c35afc1e0c08f37632feb8
GabrielAlbuquerque01 <105120066+GabrielAlbuquerque01@users.noreply.github.com>
2022-09-19 15:23:34 -0300

    menu principal, configurar e botões.

---
+++ src/jogo/Jogando.java
@@ -0,0 +1,22 @@
+package jogo;
+
+public class Jogando {
+	
+	private final MexerPeca tab = new MexerPeca(3);
+
+	public void jogo() {
+		putDados();
+		tab.mexeBaixo();
+		System.out.println("    ");
+		tab.exibeMatriz();
+	}
+	
+	
+	public void putDados(){
+		tab.preencheArray();
+		tab.embaralhaArray();
+		tab.preencheMatriz();
+		tab.exibeMatriz();
+	}
+	
+}
